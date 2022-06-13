ce0b1d1e9750eb3d73ddd778a37a74b8eb18bb02
Shraddha <mailmeatshraddha@gmail.com>
2022-06-12 22:46:09 -0400

    feat: add sticky footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <>
+    <div className="flex h-screen flex-col">
       <Header />
-    </>
+      <main className="flex-grow">Main</main>
+      <Footer />
+    </div>
   );
 }
 
