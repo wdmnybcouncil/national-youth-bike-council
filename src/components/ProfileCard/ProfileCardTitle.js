ac056d2b982ac85561af29f6cd1bd69beb23448f
Shraddha <mailmeatshraddha@gmail.com>
2022-06-14 11:31:18 -0400

    feat: add ProfileCardTitle component

---
+++ src/components/ProfileCard/ProfileCardTitle.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * The **ProfileCardTitle** component renders a title for user profile card in the webpage
+ *
+ * @version 1.0.0
+ * @author [Shraddha](https://github.com/5hraddha)
+ */
+function ProfileCardTitle({ children }) {
+  return <h3 className="text-center font-semibold uppercase text-skin-primary">{children}</h3>;
+}
+
+const propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+ProfileCardTitle.displayName = "ProfileCardTitle";
+ProfileCardTitle.propTypes = propTypes;
+
+export default ProfileCardTitle;
