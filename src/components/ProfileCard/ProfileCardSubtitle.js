a2b83518209d79cfb80cc0aab0ae45870f3f9b00
Shraddha <mailmeatshraddha@gmail.com>
2022-06-14 11:32:15 -0400

    feat: add ProfileCardSubtitle component

---
+++ src/components/ProfileCard/ProfileCardSubtitle.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * The **ProfileCardSubtitle** component renders a subtitle for user profile card in the webpage
+ *
+ * @version 1.0.0
+ * @author [Shraddha](https://github.com/5hraddha)
+ */
+function ProfileCardSubtitle({ children }) {
+  return <p className="text-center text-sm font-semibold capitalize">{children}</p>;
+}
+
+const propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+ProfileCardSubtitle.displayName = "ProfileCardSubtitle";
+ProfileCardSubtitle.propTypes = propTypes;
+
+export default ProfileCardSubtitle;
