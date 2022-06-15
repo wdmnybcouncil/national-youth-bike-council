bfc816e1acee0611d2babc8ae84fa3600987e8f1
Shraddha <mailmeatshraddha@gmail.com>
2022-06-14 23:32:13 -0400

    feat: add data file

---
+++ src/constants/data.js
@@ -0,0 +1,56 @@
+// Images for WhyTheCouncil view
+import communityImage from "../assets/images/why-the-council/community.png";
+import healthImage from "../assets/images/why-the-council/health.png";
+import educationImage from "../assets/images/why-the-council/education.png";
+import leadershipImage from "../assets/images/why-the-council/leadership.png";
+
+const data = {
+  whyTheCouncil: [
+    {
+      heading: "Community",
+      text: [
+        "Local bicycle communities are one of the main reasons that council members join our cause. These are the communities we ride in, the communities we advocate for, and the communities we seek a better youth-led future for.",
+      ],
+      hashtags: "#CommunityEngagement #CyclingAwareness #HelpingPeopleBeyondTheIndividual #CyclingAdvocacy #CommunityCycling",
+      img: {
+        src: communityImage,
+        alt: "national youth council community",
+      },
+    },
+    {
+      heading: "Health",
+      text: [
+        "We encourage a healthy mindset through healthy habits to promote mental well-being while on our bikes. It benefits us by boosting our positivity, clearing our minds, and sets a motivating mood for community engagement.",
+      ],
+      hashtags: "#Biking #Mentality #Mood #Relaxing #Engaging #Fun #Positivity #ChooseCycling #HealthyMindset",
+      img: {
+        src: healthImage,
+        alt: "national youth council health",
+      },
+    },
+    {
+      heading: "Education",
+      text: [
+        "We believe that youth have the advantage of starting early which is why we foster youth development, education, and safety with all seriousness. We teach teamwork, emotional intelligence, cycling education and much more!",
+      ],
+      hashtags: "#YouthDevelopment #BicycleSafety #EmotionalIntelligence #Curiosity #Teamwork #CycleEarly",
+      img: {
+        src: educationImage,
+        alt: "national youth council education",
+      },
+    },
+    {
+      heading: "Leadership",
+      text: [
+        "The council cultivates youth leaders by providing realistic leadership experiences and making the outcomes of our annual projects into learning lessons. Youth cyclists then exhibit leadership qualities while on the road or in the council, enhancing their ability to make quality decisions. Responsibility is developed in all of our members and is vital to staying healthy, building community, building leaders, and educating our peers.",
+      ],
+      hashtags: "#YouthLeader #RoleModel #YouthAction #YouthLeadership #PeerToPeer #TimeManagement #Dedication #Passion #CyclingGoals #YouthDevelopment #KnowingYourStrengths",
+      img: {
+        src: leadershipImage,
+        alt: "national youth council leadership",
+      },
+    },
+  ],
+};
+
+export default data;
