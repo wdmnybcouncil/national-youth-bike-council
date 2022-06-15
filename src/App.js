6b7f54b5acf3bc94a14493da228224ad9ebca7d5
Shraddha <mailmeatshraddha@gmail.com>
2022-06-14 23:54:40 -0400

    feat: create route - /why-the-council

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -23,7 +24,9 @@ function App() {
     <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-grow">
-        <WhyTheCouncil data={whyTheCouncil} />
+        <Routes>
+          <Route path="/why-the-council" element={<WhyTheCouncil data={whyTheCouncil} />} />
+        </Routes>
       </main>
       <Footer />
     </div>
