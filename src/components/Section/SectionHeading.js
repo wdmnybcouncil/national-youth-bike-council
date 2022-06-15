48c34bb097a53136bf4113174332df68d54df391
Shraddha <mailmeatshraddha@gmail.com>
2022-06-14 23:30:32 -0400

    refactor: update letter-spacing of heading

--- src/components/Section/SectionHeading.js
+++ src/components/Section/SectionHeading.js
@@ -9,7 +9,7 @@ import PropTypes from "prop-types";
  */
 function SectionHeading({ children }) {
   return (
-    <h2 className="relative mb-4 w-full border-b border-skin-accent pb-1 font-balgin text-2xl text-skin-primary before:absolute before:-bottom-1 before:block before:h-2 before:w-2 before:rounded-full before:bg-skin-fill-accent md:mb-8 md:pb-2 md:text-4xl md:before:-bottom-1.5 md:before:h-3 md:before:w-3">
+    <h2 className="relative mb-4 w-full border-b border-skin-accent pb-1 font-balgin text-2xl capitalize text-skin-primary before:absolute before:-bottom-1 before:block before:h-2 before:w-2 before:rounded-full before:bg-skin-fill-accent md:mb-8 md:pb-2 md:text-4xl md:before:-bottom-1.5 md:before:h-3 md:before:w-3 tracking-wider">
       {children}
     </h2>
   );
