e8862f5b3c64969ca76e4a3848719d9d0ec598d8
Shraddha <mailmeatshraddha@gmail.com>
2022-06-14 23:46:15 -0400

    feat: update to show an array of paragraphs

--- src/views/WhyTheCouncil.js
+++ src/views/WhyTheCouncil.js
@@ -9,6 +9,14 @@ import Section from "../components/Section";
  * @author [Shraddha](https://github.com/5hraddha)
  */
 function WhyTheCouncil({ data }) {
+  const renderSectionTexts = (texts) => (
+    texts.map((text, index) => (
+      <Section.Text key={`${index}-${text.substring(0, 10)}`}>
+        {text}
+      </Section.Text>)
+    )
+  )
+
   const renderSections = () => (
     data.map((section, index) => {
       const {
@@ -22,7 +30,7 @@ function WhyTheCouncil({ data }) {
           <Section.Heading>{heading}</Section.Heading>
           <div className="grid grid-cols-2 gap-8 sm:grid-cols-4 md:gap-16">
             <div className="col-span-2 flex flex-col gap-8">
-              <Section.Text>{text[0]}</Section.Text>
+              {renderSectionTexts(text)}
               <Section.Hashtags>{hashtags}</Section.Hashtags>
             </div>
             <div className="col-span-2 place-self-center sm:justify-self-end">
