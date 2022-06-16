6a08e65e17d4cd68d4f51d3167216f79c28ae351
Shraddha <mailmeatshraddha@gmail.com>
2022-06-16 11:26:54 -0400

    refactor: remove unnecessary margin from component

--- src/components/CTALink.js
+++ src/components/CTALink.js
@@ -9,13 +9,11 @@ import { Link } from "react-router-dom";
  * @author [Shraddha](https://github.com/5hraddha)
  */
 
-function CTALink(props) {
-  const { linkTo, children } = props;
-
+function CTALink({ linkTo, children, className = "" }) {
   return (
     <Link
       to={linkTo}
-      className="focus:ring-offset-0.5 ml-2 mt-2 inline-block max-w-fit transform rounded-full bg-skin-button-accent px-6 py-2 text-sm uppercase tracking-wide text-skin-muted drop-shadow-md transition hover:-translate-y-0.5 hover:bg-skin-button-accent-hover focus:outline-none focus:ring-1 focus:ring-white lg:mt-0 lg:ml-4 lg:text-base"
+      className={`focus:ring-offset-0.5 inline-block max-w-fit transform rounded-full bg-skin-button-accent px-6 py-2 text-sm uppercase tracking-wide text-skin-muted drop-shadow-md transition hover:-translate-y-0.5 hover:bg-skin-button-accent-hover focus:outline-none focus:ring-1 focus:ring-white lg:text-base ${className}`}
     >
       {children}
     </Link>
@@ -31,6 +29,7 @@ const propTypes = {
    * Sets the content of the CTALink
    */
   children: PropTypes.any.isRequired,
+  className: PropTypes.string,
 };
 
 CTALink.displayName = "CTALink";
