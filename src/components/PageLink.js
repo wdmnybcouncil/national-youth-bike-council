596a1914480a15c04920994084e43f79ffda2360
Shraddha <mailmeatshraddha@gmail.com>
2022-06-22 11:27:26 -0400

    refactor: refactor the PageLink component

--- src/components/PageLink.js
+++ src/components/PageLink.js
@@ -3,31 +3,51 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 /**
- * The **FooterLink** component renders links in the footer
+ * The **PageLink** component renders links in the webpage
  *
  * @version 1.0.0
  * @author [Shraddha](https://github.com/5hraddha)
  */
-function FooterLink({ linkTo, children }) {
-  return (
-    <Link to={linkTo} className="flex gap-2 decoration-skin-accent underline-offset-4 transition-all hover:underline hover:opacity-90">
-      {children}
-    </Link>
-  );
+function PageLink({ type, linkTo, children }) {
+  if (type === 'internal') {
+    return (
+      <Link to={linkTo} className="flex gap- 2 mt-4 underline underline-offset-4 transition-all hover:decoration-skin-accent hover:opacity-90">
+        {children}
+      </Link>
+    );
+  }
+  if (type === 'external') {
+    return (
+      <a
+        href={linkTo}
+        target="_blank"
+        rel="noreferrer"
+        className="flex gap- 2 mt-4 underline underline-offset-4 transition-all hover:decoration-skin-accent hover:opacity-90"
+      >
+        {children}
+      </a>
+    );
+  }
+
+  return null;
 }
 
 const propTypes = {
   /**
-   * Sets the url where the FooterLink should point to when clicked
+  * Defines the type of the link: whether it is linking to an external page or internal page
+  */
+  type: PropTypes.oneOf(['external', 'internal']).isRequired,
+  /**
+   * Sets the url where the PageLink should point to when clicked
    */
   linkTo: PropTypes.string.isRequired,
   /**
-   * Sets the content or text of the FooterLink
+   * Sets the content or text of the PageLink
    */
   children: PropTypes.any.isRequired,
 };
 
-FooterLink.displayName = "FooterLink";
-FooterLink.propTypes = propTypes;
+PageLink.displayName = "PageLink";
+PageLink.propTypes = propTypes;
 
-export default FooterLink;
+export default PageLink;
