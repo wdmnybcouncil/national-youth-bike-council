8030dc8fe6d907f5dfc3383ac85ca60ad08aae4a
Shraddha <mailmeatshraddha@gmail.com>
2022-07-18 14:33:19 -0400

    feat: setup strapi with postgres

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      schema: env('DATABASE_SCHEMA'), // Not required
+      ssl: env.json({
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      }),
+    },
+    debug: false,
+  },
+});
