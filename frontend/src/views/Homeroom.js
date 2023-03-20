14ca915c52f79e282a4d75fcc13afe3e4d1f03c4
Shraddha <mailmeatshraddha@gmail.com>
2023-03-19 21:10:59 -0400

    feat: add Homeroom view

---
+++ frontend/src/views/Homeroom.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Section from "../components/Section";
+import api from "../utils/api";
+
+/**
+ * The **Homeroom** component renders the educational material of the council.
+ *
+ * @version 1.0.0
+ * @author [Shraddha](https://github.com/5hraddha)
+ */
+function Homeroom() {
+  return (
+    <div className="my-8" aria-label="resources page">
+      <Section>
+        <Section.Heading>Homeroom</Section.Heading>
+      </Section>
+    </div>
+  );
+}
+
+Homeroom.displayName = "Homeroom";
+
+export default Homeroom;
